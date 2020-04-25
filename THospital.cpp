80d1ddb6f56a2a5e59e5711335ff6bca0084009e
AndrescorporationSL <61791595+AndrescorporationSL@users.noreply.github.com>
2020-04-25 17:59:59 +0200

    Update THospital.cpp

--- THospital.cpp
+++ THospital.cpp
@@ -3,20 +3,47 @@
 
 #include <stdio.h>
 
+int menu();
+{
+  char opcion;
+  printf("A.Acceder a consulta\n");
+  printf("B.Buscar datos de un paciente\n");
+  printf("C.Acceder a Urgencias\n");
+  printf("D.Salir\n");
+  scanf("%c",&opcion);
+  
+  do{
+  switch(opcion)
+    case'A':case'a':
+      return 0;
+  break;
+  case'B':case'b':
+      return 1;
+  break;
+  case'C':case'c':
+      return 2;
+  break;
+  case'D':case'd':
+      return 3;
+  break;
+  default:
+     printf("Escoja una opci%cn v%clida",162,160);
+  }while(opcion!='A' || opcion!='a' || opcion!='B' || opcion!='b' || opcion!='C' || opcion!='c' || 
+ 
+  
+  
+}
+  
 int Introduccion()
 {
   printf("Buenos d%cas, se encuentra en la recepci%cn del hospital ABRM, %cQu%c desea hacer?\n",161,162,168,130);
-  switch(opcion)
-  {
-    case '1':
-      //
-      
+  
       
 }
 int main()
 {
   int bucle=0;
-  int opcion;
+
  
   
   
