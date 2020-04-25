6355f078ef2291084d41fe85c3846e394e418576
mariooot13 <61791485+mariooot13@users.noreply.github.com>
2020-04-25 12:49:00 +0200

    Update THospital.cpp

--- THospital.cpp
+++ THospital.cpp
@@ -1,3 +1,23 @@
 //TRABAJO RECEPCION DE HOSPITAL
-//PARTICIPANTES: Mario Sánchez García, Brian Tomico Fernández, Raul ricaldi, Andres Torrejón Palomo
+//PARTICIPANTES: Mario Sánchez García (55452), Brian Tomico Fernández (55487), Raul Ricaldi Esquivel (55423), Andres Torrejón Palomo (55489).
+
 #include <stdio.h>
+
+int Introduccion()
+{
+  printf("Buenos d%cas, se encuentra en la recepci%cn del hospital ABRM,%cQu%c desea hacer?\n",161,162,168,130);
+  switch(opcion)
+  {
+    case '1':
+      //
+      
+      
+}
+int main()
+{
+  int bucle=0;
+  int opcion;
+ 
+  
+  
+  
