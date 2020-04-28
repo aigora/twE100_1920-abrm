743fff4d3e65fcae77b21eba0f3ab459b78532e5
mariooot13 <61791485+mariooot13@users.noreply.github.com>
2020-04-28 13:37:30 +0200

    Update THospital.cpp

--- THospital.cpp
+++ THospital.cpp
@@ -3,12 +3,24 @@
 
 #include <stdio.h>
 #include<string.h>
+#define N 100
+
+typedef struct
+{
+	char nombre[N];
+	char apellido1[N];
+	char apellido2[N];
+	char fecha;
+	char DNI[N];
+}TPaciente;
 
 int Introduccion(void);
 void LimpiarBufer (void);    //Esta fucnión limpia el bufer
 int Menu();		      //Esta función abre el menu de opciones disponibles en la recepción
 void FCierre (void);	      //Esta función se usa al finalizar el programa
 int FSalir();
+void FBusqueda();
+void FCompararEleccion(int eleccion);
 
 int main()
 {
@@ -18,17 +30,15 @@ int main()
 	Introduccion();
 	Menu();
 	eleccion=Menu();
-		
-	if(eleccion==3){
-		bucle=Salir();
+	FCompararEleccion(eleccion);
+	if(eleccion==4)
+	{
+		break;
 	}
-		
 	}while(bucle!=0);
 	
 	FCierre();
-	return 69;
-		
-	
+	return 69;	
 }
 
 int Introduccion()
@@ -38,51 +48,63 @@ int Introduccion()
 
 int Menu()
 {
-  char opcion;
+  int opcion;
   do{
-  printf("A.Acceder a consulta\n");
-  printf("B.Buscar datos de un paciente\n");
-  printf("C.Acceder a Urgencias\n");
-  printf("D.Salir\n");
-  scanf("%c",&opcion);
-
-  switch(opcion)//En este switch se elige la opcion que se desea
-  {
-    case'A':case'a':
-      return 0;
-  break;
-  case'B':case'b':
-      return 1;
-  break;
-  case'C':case'c':
-      return 2;
-  break;
-  case'D':case'd':
-      return 3;
-  break;
-  default:
-     printf("Escoja una opci%cn v%clida",162,160);
-  }
-  }while(opcion!='A' || opcion!='a' || opcion!='B' || opcion!='b' || opcion!='C' || opcion!='c' || opcion!='D' || opcion!='d');
+  printf("1.Acceder a consulta\n");
+  printf("2.Buscar datos de un paciente\n");
+  printf("3.Acceder a Urgencias\n");
+  printf("4.Salir\n");
+  scanf("%d",&opcion);
+  if(opcion!= 1 || opcion!= 2 || opcion!= 3 || opcion!= 4)
+  { printf("Opcion no valida, escoja otra opcion por favor\n");}
+  }while(opcion!= 1 || opcion!= 2 || opcion!= 3 || opcion!= 4);
+return opcion;
 }
 
-float CompararEleccion (int eleccion)
+void FCompararEleccion(int eleccion)	
 {
-switch(eleccion)
+	switch(eleccion)
+	{
+		case 1:
+		FConsulta();
+		break;
+		case 2:
+		FBusqueda();
+		break;
+		case 3:
+		FUrgencias();
+		break;
+		case 4:
+		FSalir();
+		break;     
+	}
+}
+
+void FBusqueda()
 {
-	case 0:
-	FConsulta();
-	break;
-	case 1:
-	FHistorial();
-	break;
-	case 2:
-	FUrgencias();
-	break;
-	case 3:
-	FSalir();
-	break;
-}}
+	TPaciente c;
+	char a;
+	printf("Dígame el DNI de la persona que busca por favor\n");
+	scanf("%c",&c.dni);
+	FILE* pf;
+	pf = fopen("Datoshospital.txt","r");
+	while(a!= EOF)
+	{
+		a = fgets(pf);
+		if(a==c.dni)
+		{
+			printf("El paciente que estás buscando es %c %c %c con DNI %c y fecha de entrada %c\n",c.nombre,c.apellido1,
+			       c.apellido2,c.dni,c.fecha); }
+		else 
+			printf("El paciente que busca no ha sido ingresado en este hospital, lo sentimos...");
+	}
+}
+	
+	
+	
+	
+	
+	
 	
 int FSalir()
 {
